3f61d5c6f33b166188d37f5edcb60e8732466477
Soh Jun Wen <mail.junwen@gmail.com>
2025-04-12 22:35:51 +0800

    Create basic layout structure for the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import styled from "styled-components";
 import "./App.css";
 import ToolBar from "./components/ToolBar/ToolBar";
+import ControlPanel from "./components/ControlPanel/ControlPanel";
 
 function App() {
   return (
-    <>
+    <Layout>
       <ToolBar />
-    </>
+      <Body>
+        <ControlPanel />
+        <CanvasPlaceholder>{/* Future DrawingArea */}</CanvasPlaceholder>
+      </Body>
+    </Layout>
   );
 }
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const Body = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const CanvasPlaceholder = styled.div`
+  flex: 1;
+  background: #eee;
+`;
 
 export default App;
