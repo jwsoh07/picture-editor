3a2e3f04339335abf1257b5683e3bcb223e18967
Soh Jun Wen <mail.junwen@gmail.com>
2025-04-13 17:51:17 +0800

    Update ToolBar to use react-icons for tool selection

--- src/components/ToolBar/ToolBar.tsx
+++ src/components/ToolBar/ToolBar.tsx
@@ -1,12 +1,23 @@
 import styled from "styled-components";
+import { PiShapesLight } from "react-icons/pi";
+import { IoColorFillOutline } from "react-icons/io5";
 import { observer } from "mobx-react-lite";
 
 import toolStore from "../../stores/ToolStore";
+import { ICON_SIZE } from "../../constants";
 
 const ToolBar = observer(() => {
   const tools = [
-    { id: "shape", label: "Shape" },
-    { id: "fill", label: "Fill" },
+    {
+      id: "shape",
+      label: "Shape tool",
+      icon: <PiShapesLight size={ICON_SIZE} />,
+    },
+    {
+      id: "fill",
+      label: "Fill tool",
+      icon: <IoColorFillOutline size={ICON_SIZE} />,
+    },
   ] as const;
 
   return (
@@ -16,8 +27,9 @@ const ToolBar = observer(() => {
           key={tool.id}
           $active={toolStore.selectedTool === tool.id}
           onClick={() => toolStore.setSelectedTool(tool.id)}
+          aria-label={tool.label}
         >
-          {tool.label}
+          {tool.icon}
         </ToolButton>
       ))}
     </ToolbarWrapper>
@@ -27,22 +39,18 @@ const ToolBar = observer(() => {
 const ToolbarWrapper = styled.div`
   display: flex;
   justify-content: start;
-  padding: 10px;
-  background-color: #333;
-  gap: 10px;
+  padding: 4px 6px;
+  background-color: #fcfcfc;
+  gap: 2px;
 `;
 
 const ToolButton = styled.button<{ $active: boolean }>`
-  background-color: ${(props) => (props.$active ? "#555" : "#777")};
-  color: white;
-  padding: 8px 12px;
+  background-color: transparent;
   border: none;
+  color: ${(props) => (props.$active ? "rgb(255, 0, 0)" : "#333")};
+  padding: 4px 8px;
   border-radius: 4px;
   cursor: pointer;
-
-  &:hover {
-    background-color: #999;
-  }
 `;
 
 export default ToolBar;
