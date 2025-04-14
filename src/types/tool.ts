d7e87f5c7833b693171a3b48bac0783b82cf4dc1
Soh Jun Wen <mail.junwen@gmail.com>
2025-04-14 15:02:57 +0800

    UI enhancement for Toolbar

--- src/types/tool.ts
+++ src/types/tool.ts
@@ -1,6 +1,6 @@
 export type ToolID = "shape" | "fill" | null;
 
-export type Shape = "rectangle" | "circle";
+export type Shape = "rectangle" | "circle" | "hexagon" | "triangle";
 
 export type ShapeToolOptions = {
   type: Shape;
